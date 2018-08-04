86c5caf7aef45798d86ed30bca5fe864fc78690e
wjdgns7989 <wjdgns7989@naver.com>
2018-08-04 14:15:01 +0900

    Completed Json testing

--- BlockchainCertificateProject/src/main/java/com/tomato/controller/CertificationController.java
+++ BlockchainCertificateProject/src/main/java/com/tomato/controller/CertificationController.java
@@ -23,42 +23,26 @@ import org.json.simple.parser.ParseException;
 @Controller
 public class CertificationController {
 
-	@RequestMapping(value = "/boxcheck.do")
-	String boxCheck(HttpServletRequest request) {
-		String[] value = request.getParameterValues("checkbox");
-		System.out.println(Arrays.toString(value));
-		
-		JSONParser parser = new JSONParser();
-		try {
-	 
-			Object obj1 = parser.parse(new FileReader("halin.json"));
-			Object obj2 = parser.parse(new FileReader("halin.json"));
-	 
-			JSONObject jsonObject1 = (JSONObject) obj1;
-			JSONObject jsonObject2 = (JSONObject) obj2;
-	 
-			JSONObject name = (JSONObject) jsonObject1.get("재학");
-			JSONObject temp = (JSONObject) jsonObject2.get("이름");
-			
-			
-			System.out.println(temp);
-	 
-			
-			
-	 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-	 
-	     
+   @RequestMapping(value = "/boxcheck.do")
+   String boxCheck(HttpServletRequest request) {
+      String[] value = request.getParameterValues("checkbox");
+      System.out.println(Arrays.toString(value));
+      
+      JSONParser parser = new JSONParser(); 
+      try { 
+         Object obj = parser.parse(new FileReader("halin.json")); 
+         JSONObject jsonObject = (JSONObject) obj;
+         JSONObject dataObject = (JSONObject) jsonObject.get("certificate1"); 
+         System.out.println("type : " +dataObject.get("type"));    
+      } catch (FileNotFoundException e) { 
+         e.printStackTrace(); 
+      } catch (IOException e) { 
+         e.printStackTrace();
+      } catch (ParseException e) { 
+         e.printStackTrace(); 
+      }
+      
 
-
-	
-		
-		return "result";
-	}
+      return "result";
+   }
 }
--- BlockchainCertificateProject/src/main/java/com/tomato/email/EmailSender.java
+++ BlockchainCertificateProject/src/main/java/com/tomato/email/EmailSender.java
@@ -24,8 +24,37 @@ public class EmailSender  {
        MimeMessage msg = mailSender.createMimeMessage();
         try {
             msg.setSubject(email.getSubject());
-            msg.setText("<b>홍길동 졸업 증명서 발급<br></b> "+
-                      "<a href=\"https://www.naver.com/\">인증</a>", "UTF-8", "html");
+            msg.setText("<h2>CSS Layout Float</h2>\r\n" + 
+                  "<p>In this example, we have created a header, two columns/boxes and a footer. On smaller screens, the columns will stack on top of each other.</p>\r\n" + 
+                  "<p>Resize the browser window to see the responsive effect (you will learn more about this in our next chapter - HTML Responsive.)</p>\r\n" + 
+                  "\r\n" + 
+                  "<header>\r\n" + 
+                  "  <h2>Cities</h2>\r\n" + 
+                  "</header>\r\n" + 
+                  "\r\n" + 
+                  "<section>\r\n" + 
+                  "  <nav>\r\n" + 
+                  "    <ul>\r\n" + 
+                  "      <li><a href=\"#\">London</a></li>\r\n" + 
+                  "      <li><a href=\"#\">Paris</a></li>\r\n" + 
+                  "      <li><a href=\"#\">Tokyo</a></li>\r\n" + 
+                  "    </ul>\r\n" + 
+                  "  </nav>\r\n" + 
+                  "  \r\n" + 
+                  "  <article style=\"float: left;\r\n" + 
+                  "    padding: 20px;\r\n" + 
+                  "    width: 70%;\r\n" + 
+                  "    background-color: #f1f1f1;\r\n" + 
+                  "    height: 300px;\">\r\n" + 
+                  "    <h1>London</h1>\r\n" + 
+                  "    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>\r\n" + 
+                  "    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>\r\n" + 
+                  "  </article>\r\n" + 
+                  "</section>\r\n" + 
+                  "\r\n" + 
+                  "<footer>\r\n" + 
+                  "  <p>Footer</p>\r\n" + 
+                  "</footer>", "UTF-8", "html");
             msg.setRecipients(MimeMessage.RecipientType.TO , InternetAddress.parse(email.getReciver()));
             msg.setFrom("sejong");
             
